7cb59e6472f835121d0a7616e7d04014a2990fe4
Nat <1991.ntm@gmail.com>
2025-06-24 16:51:46 -0700

    updated Connectwise API part

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -1,5 +1,14 @@
 from flask import Flask, request, jsonify
 import json
+import os
+from pyconnectwise import ConnectWiseManageAPIClient
+
+company_id = os.getenv("company_id")
+manage_url = os.getenv("manage_url")
+client_id = os.getenv("client_id")
+public_key = os.getenv("public_key")
+private_key = os.getenv("private_key")
+manage_api_client = ConnectWiseManageAPIClient(company_id, manage_url, client_id, public_key, private_key)
 
 app = Flask(__name__)
 
@@ -24,10 +33,17 @@ def handle_webhook():
 
     # Extract ticket title
     ticket_title = entity.get("summary", "").lower()
+    ticket_id = entity.get("id", "")
 
     if "voicemail for" in ticket_title:
         print("Voicemail ticket detected:", ticket_title)
-        # Run urgency model, store result, etc.
+        # todo need to call API to extract the note
+        ticket_initial_note = manage_api_client.service.tickets.id(ticket_id).notes.get()[0]
+        print("Detail:", ticket_initial_note)
+
+        # todo need to call Vertex API to detect urgency
+
+        # todo need to call Twilio API to send text
         return jsonify({"status": "processed"}), 200
     else:
         print("Skipping non-voicemail ticket:", ticket_title)
