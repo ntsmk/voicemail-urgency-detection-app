aaec2b127964e0309a49b42b61e19f32d1d4a776
Nat <1991.ntm@gmail.com>
2025-07-04 15:59:17 -0700

    working on test_notes

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -77,22 +77,23 @@ def test_notes(ticket_id):
     import base64
 
     cw_company_id = os.getenv("company_id")
-    cw_manage_url = os.getenv("manage_url")
     cw_client_id = os.getenv("client_id")
     cw_public_key = os.getenv("public_key")
     cw_private_key = os.getenv("private_key")
+    # cw_manage_url = os.getenv("manage_url")
 
     auth_str = f"{cw_company_id}+{cw_public_key}:{cw_private_key}"
     auth_bytes = auth_str.encode('ascii')
     auth_b64 = base64.b64encode(auth_bytes).decode('ascii')
 
-    base_url = f"https://{cw_manage_url}/v2022_1/apis/3.0"
+    base_url = "https://na.myconnectwise.net/v4_6_release/apis/3.0"
+    note_url = f"{base_url}/service/tickets/{ticket_id}/notes"
     headers = {
         "clientId": cw_client_id,
         "Authorization": f"Basic {auth_b64}",
         "Content-Type": "application/json"
     }
-    note_url = f"{base_url}/service/tickets/{ticket_id}/notes"
+
 
     try:
         response = requests.get(note_url, headers=headers)
