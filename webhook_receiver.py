31a3ee43d48f6d3c5332cee656d6c319821129bc
Nat <1991.ntm@gmail.com>
2025-07-08 16:54:39 -0700

    modifying webhook function

--- webhook_receiver.py
+++ webhook_receiver.py
@@ -55,6 +55,49 @@ def handle_webhook():
                 notes = response.json()
                 if notes:
                     print("First Note:", notes[0])
+                    split_note = notes[0]["text"].split("--- Google transcription result ---", 1)[-1].strip()
+                    if split_note not in ["(Google was unable to recognize any speech in audio data.)", "null",
+                                          "null\nnull"]:
+                        trimmed_note = split_note
+                        # todo connect to Vertex AI API here
+                        gcp_project_id = os.getenv("project_id")
+                        gcp_location = os.getenv("location")
+                        gcp_endpoint_id = os.getenv("endpoint_id")
+                        URL = f"https://{gcp_location}-aiplatform.googleapis.com/v1/projects/{gcp_project_id}/locations/{gcp_location}/endpoints/{gcp_endpoint_id}:generateContent"
+                        print("set credentials for machine learning")
+
+                        # Get access token using default credentials (Application Default Credentials)
+                        credentials, _ = default(scopes=["https://www.googleapis.com/auth/cloud-platform"])
+                        credentials.refresh(Request())
+                        token = credentials.token
+
+                        # Build the headers
+                        headers = {
+                            "Authorization": f"Bearer {token}",
+                            "Content-Type": "application/json"
+                        }
+
+                        # Gemini content format
+                        body = {
+                            "contents": [
+                                {
+                                    "role": "user",
+                                    "parts": [
+                                        {"text": f"{trimmed_note}"}
+                                    ]
+                                }
+                            ],
+                        }
+
+                        # Make the POST request
+                        response = requests.post(URL, headers=headers, json=body)
+
+                        # Show result
+                        print(response.json()['candidates'][0]['content']['parts'][0]['text'])
+
+                    else:
+                        trimmed_note = "the record is empty"
+                    print("Note passed to machine learning:", trimmed_note)
                 else:
                     print("No notes found.")
             else:
